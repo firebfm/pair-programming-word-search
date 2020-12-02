ea49255518a01882a8c50bd815c9255ac50d49a8
Simon Huang <firebfm@gmail.com>
2020-12-02 22:38:19 +0000

    test cases, vertical solve with transpose

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
+function transpose(matrix){
+    let newArray = [];
+    for(let i = 0; i < matrix[0].length; i++){
+        newArray.push([]);
+    };
+  
+    for(let i = 0; i < matrix[0].length; i++){
+        for(let j = 0; j < matrix.length; j++){
+            newArray[i].push(matrix[j][i]);
+        };
+    };
+  
+    return newArray;
+  }
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (word && l.includes(word)) {
+            console.log('HORIZONTAL IS ' + word);
+            return true;
+        }
     }
+    
+    if (letters.length > 0) {
+        const verticalMatrix = transpose(letters);
+        const verticalJoin = verticalMatrix.map(ls => ls.join(''));
+        for (let l of verticalJoin) {
+            if (word && l.includes(word)) {
+                console.log('VERTICAL is ' + word)
+                return true;
+            }
+        }
+    } 
+    
+    return false;
 }
 
 module.exports = wordSearch
